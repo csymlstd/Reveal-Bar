a6024440a00acba0f00ac47bba6e35c19d3d1618
Mur <austin@forcir.com>
2017-02-16 09:11:38 -0800

    improve indentation, fix spelling issue

--- revealbar.js
+++ revealbar.js
@@ -1,188 +1,188 @@
 /**
- *
- * Reveal Bar (vanilla JS)
- * based on https://github.com/DesignerNews/Reveal-Bar
- *
- * @author Nick Hartskeerl <apachenick@hotmail.com>
- *
- */
-var reavealbar = function(query, _options) {
+*
+* Reveal Bar (vanilla JS)
+* based on https://github.com/DesignerNews/Reveal-Bar
+*
+* @author Nick Hartskeerl <apachenick@hotmail.com>
+*
+*/
+var revealbar = function(query, _options) {
 
-    function offset(elem) {
+  function offset(elem) {
 
-        var doc, docElem, rect, win;
+    var doc, docElem, rect, win;
 
-        if(!elem ) {
-            return;
+    if(!elem ) {
+      return;
+    }
+
+    if(!elem.getClientRects().length) {
+      return { top: 0, left: 0 };
+    }
+
+    rect = elem.getBoundingClientRect();
+
+    doc = elem.ownerDocument;
+    docElem = doc.documentElement;
+    win = doc.defaultView;
+
+    return {
+      top: rect.top + win.pageYOffset - docElem.clientTop,
+      left: rect.left + win.pageXOffset - docElem.clientLeft
+    };
+
+  }
+
+  function css(element, selector) {
+
+    if(typeof selector == "object") {
+
+      for(var key in selector) {
+
+        if(!selector.hasOwnProperty(key)) {
+          continue;
         }
 
-        if(!elem.getClientRects().length) {
-            return { top: 0, left: 0 };
-        }
+        var value = selector[key];
 
-        rect = elem.getBoundingClientRect();
+        key = key.replace(/-([a-z0-9_])/g, function(string, letter) {
+          return letter.toUpperCase();
+        }).replace("-", "");
 
-        doc = elem.ownerDocument;
-        docElem = doc.documentElement;
-        win = doc.defaultView;
+        element.style[key] = value;
 
-        return {
-            top: rect.top + win.pageYOffset - docElem.clientTop,
-            left: rect.left + win.pageXOffset - docElem.clientLeft
+      }
+
+      return;
+
+    }
+
+    var value = window.getComputedStyle(element).getPropertyValue(selector);
+
+    return value;
+
+  }
+
+  function outerHeight(element) {
+    return element.offsetHeight;
+  }
+
+  function height(element) {
+
+    if(element == window || element == document) {
+      return element.innerHeight;
+    }
+
+    var height = css(element, "height");
+
+    return parseFloat(height);
+
+  }
+
+  var $bar = document.querySelector(query);
+
+  var noop = function() {
+
+  };
+
+  var options = {
+    onAttach: noop,
+    onDetach: noop,
+    onShow: noop,
+    onHide: noop,
+    bottomOffset: 0
+  };
+
+  for(var key in _options) {
+    if(options.hasOwnProperty(key)) {
+      options[key] = _options[key];
+    }
+  }
+
+  var $window = window,
+  $document = document,
+  lastPosition = window.pageYOffset,
+  initialBarPosition = offset($bar).top,
+  isiOS = /(iPad|iPhone|iPod)/g.test(navigator.userAgent);
+
+  if (isiOS) return;
+
+  var scroll = function(e) {
+
+    var currentPosition = window.pageYOffset,
+    barFullHeight = outerHeight($bar),
+    barOffsetHeight = barFullHeight - options.bottomOffset,
+    barTopPosition = offset($bar).top;
+
+    if(currentPosition < 0 || currentPosition > (height($document) - height($window))) {
+      return;
+    }
+
+    if(currentPosition < lastPosition) {
+
+      if (currentPosition === 0) {
+        options.onAttach();
+      } else {
+
+        if(css($bar, 'position') === 'fixed' && currentPosition > barTopPosition) {
+
+          css($bar, {
+            'position': 'absolute',
+            'top': (barTopPosition + 1)+'px'
+          });
+
+        } else if(css($bar, 'position') === 'absolute' && currentPosition <= barTopPosition) {
+
+          options.onShow();
+
+          css($bar, {
+            'position': 'fixed',
+            'top': '0px'
+          });
+
         };
 
-    }
+      }
 
-    function css(element, selector) {
+    } else if(currentPosition > lastPosition || (currentPosition === lastPosition && currentPosition !== 0)) {
 
-        if(typeof selector == "object") {
+      if(currentPosition > barTopPosition + barOffsetHeight) {
 
-            for(var key in selector) {
-
-                if(!selector.hasOwnProperty(key)) {
-                    continue;
-                }
-
-                var value = selector[key];
-
-                key = key.replace(/-([a-z0-9_])/g, function(string, letter) {
-                    return letter.toUpperCase();
-                }).replace("-", "");
-
-                element.style[key] = value;
-
-            }
-
-            return;
-
-        }
-
-        var value = window.getComputedStyle(element).getPropertyValue(selector);
-
-        return value;
-
-    }
-
-    function outerHeight(element) {
-        return element.offsetHeight;
-    }
-
-    function height(element) {
-
-        if(element == window || element == document) {
-            return element.innerHeight;
-        }
-
-        var height = css(element, "height");
-
-        return parseFloat(height);
-
-    }
-
-    var $bar = document.querySelector(query);
-
-    var noop = function() {
-
-    };
-
-    var options = {
-        onAttach: noop,
-        onDetach: noop,
-        onShow: noop,
-        onHide: noop,
-        bottomOffset: 0
-    };
-
-    for(var key in _options) {
-        if(options.hasOwnProperty(key)) {
-            options[key] = _options[key];
-        }
-    }
-
-    var $window = window,
-        $document = document,
-        lastPosition = window.pageYOffset,
-        initialBarPosition = offset($bar).top,
-        isiOS = /(iPad|iPhone|iPod)/g.test(navigator.userAgent);
-
-    if (isiOS) return;
-
-    var scroll = function(e) {
-
-        var currentPosition = window.pageYOffset,
-            barFullHeight = outerHeight($bar),
-            barOffsetHeight = barFullHeight - options.bottomOffset,
-            barTopPosition = offset($bar).top;
-
-        if(currentPosition < 0 || currentPosition > (height($document) - height($window))) {
-            return;
-        }
-
-        if(currentPosition < lastPosition) {
-
-            if (currentPosition === 0) {
-                options.onAttach();
-            } else {
-
-                if(css($bar, 'position') === 'fixed' && currentPosition > barTopPosition) {
-
-                    css($bar, {
-                        'position': 'absolute',
-                        'top': (barTopPosition + 1)+'px'
-                    });
-
-                } else if(css($bar, 'position') === 'absolute' && currentPosition <= barTopPosition) {
-
-                    options.onShow();
-
-                    css($bar, {
-                      'position': 'fixed',
-                      'top': '0px'
-                    });
-
-                };
-
-            }
-
-        } else if(currentPosition > lastPosition || (currentPosition === lastPosition && currentPosition !== 0)) {
-            
-            if(currentPosition > barTopPosition + barOffsetHeight) {
-
-                options.onDetach();
-                options.onHide();
-
-                css($bar, {
-                    'position': 'fixed',
-                    'top': -barOffsetHeight+'px'
-                });
-
-            } else if (currentPosition !== barTopPosition + barOffsetHeight) {
-
-                css($bar, {
-                    'position': 'absolute',
-                    'top': barTopPosition+'px'
-                });
-
-            };
-
-        }
-
-        lastPosition = currentPosition;
-
-    };
-
-    scroll(window.event);
-    window.addEventListener("scroll", scroll);
-
-    this.destroy = function() {
-
-        options.onAttach();
+        options.onDetach();
+        options.onHide();
 
         css($bar, {
-            'position': 'absolute',
-            'top': initialBarPosition+'px'
+          'position': 'fixed',
+          'top': -barOffsetHeight+'px'
         });
 
-    };
+      } else if (currentPosition !== barTopPosition + barOffsetHeight) {
+
+        css($bar, {
+          'position': 'absolute',
+          'top': barTopPosition+'px'
+        });
+
+      };
+
+    }
+
+    lastPosition = currentPosition;
+
+  };
+
+  scroll(window.event);
+  window.addEventListener("scroll", scroll);
+
+  this.destroy = function() {
+
+    options.onAttach();
+
+    css($bar, {
+      'position': 'absolute',
+      'top': initialBarPosition+'px'
+    });
+
+  };
 
 };
